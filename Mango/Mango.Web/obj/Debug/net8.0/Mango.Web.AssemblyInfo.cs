13a67e1caeec262352b74e2e53e344ee95cea9be
Bojan <skopljak.pr117.2020@uns.ac.rs>
2025-03-06 13:55:37 +0100

    Home page update

--- Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329b0d60fc51e5b9f4d783571fc6c6d9d9e2d100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7fdfbff03f198fc2e3a9269fd7bec90ecfd3db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
