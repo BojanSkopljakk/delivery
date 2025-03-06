3f7fdfbff03f198fc2e3a9269fd7bec90ecfd3db
Bojan <skopljak.pr117.2020@uns.ac.rs>
2025-03-06 13:23:10 +0100

    ProductAPI

--- Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ed13bc7743fe0e12304ca97dd49b5eeb15f091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329b0d60fc51e5b9f4d783571fc6c6d9d9e2d100")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
