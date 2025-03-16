2ed28f952f4c0e4a22cd4d19d25a2ba500b1a629
Bojan <skopljak.pr117.2020@uns.ac.rs>
2025-03-16 17:36:28 +0100

    ocelot gateway config

--- Mango/Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12be82cc65028cf916ebd5717bff933022ee2838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ded6845504162ab784f1f9650f51be741196e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango/Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c4de396afaa98a2fd8ec9fc59fae5cb423a098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ded6845504162ab784f1f9650f51be741196e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
